c7588e8a19b85c911fb28defba976fd5071dadf0
Zachary Dovel <zacharyd@slalom.com>
2015-05-11 18:40:51 -0700

    Fixes

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,10 +3,12 @@
 # Class for the supervisord service
 #
 class supervisord::service inherits supervisord  {
-  service { $supervisord::service_name:
-    ensure     => $supervisord::service_ensure,
-    enable     => $supervisord::package_enable,
-    hasrestart => true,
-    hasstatus  => true
+  if $supervisord::service_manage == true {
+    service { $supervisord::service_name:
+      ensure     => $supervisord::service_ensure,
+      enable     => $supervisord::package_enable,
+      hasrestart => true,
+      hasstatus  => true
+    }
   }
 }
